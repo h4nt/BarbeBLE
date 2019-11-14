7eb8435a1928552f97241da8eb82f46092524fd9
NGUYEN Thai Ha <thai-ha.nguyen@horanet.com>
2019-11-14 17:30:53 +0100

    add android project

---
+++ app/src/main/java/com/horanet/barbeble/MainActivity.java
@@ -0,0 +1,17 @@
+package com.horanet.barbeble;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.tv);
+        tv.setText("Hello Polytech");
+    }
+}
