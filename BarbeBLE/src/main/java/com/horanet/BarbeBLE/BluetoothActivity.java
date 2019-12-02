636c9a8a599eddb96a33dcc8faf5166739aaeb0d
storiolo <nicosto40@gmail.com>
2019-12-02 17:59:12 +0100

    New version du BLE qui fonctionne

---
+++ BarbeBLE/src/main/java/com/horanet/BarbeBLE/BluetoothActivity.java
@@ -0,0 +1,102 @@
+package com.horanet.BarbeBLE;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+/**
+ * Created by itanbarpeled on 28/01/2018.
+ */
+
+public abstract class BluetoothActivity extends AppCompatActivity {
+
+
+    private Toolbar mToolbar;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutId());
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setToolbar();
+    }
+
+
+    protected abstract int getLayoutId();
+
+    protected abstract int getTitleString();
+
+    protected void onBackButtonClicked() {
+        onBackPressed();
+    }
+
+    protected void showMsgText(int stringId) {
+        showMsgText(getString(stringId));
+    }
+
+    protected void showMsgText(String string) {
+        if (mToolbar != null) {
+            Snackbar snackbar = Snackbar.make(mToolbar, string, Snackbar.LENGTH_LONG);
+            snackbar.show();
+        }
+    }
+
+    protected BluetoothAdapter getBluetoothAdapter() {
+
+        BluetoothAdapter bluetoothAdapter;
+        BluetoothManager bluetoothService = ((BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE));
+
+        if (bluetoothService != null) {
+
+            bluetoothAdapter = bluetoothService.getAdapter();
+
+            // Is Bluetooth supported on this device?
+            if (bluetoothAdapter != null) {
+
+                // Is Bluetooth turned on?
+                if (bluetoothAdapter.isEnabled()) {
+                    /*
+                    all the other Bluetooth initial checks already verified in MainActivity
+                     */
+                    return bluetoothAdapter;
+                }
+            }
+        }
+
+        return null;
+    }
+
+
+    private void setToolbar() {
+        setSupportActionBar(mToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(false);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
+
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackButtonClicked();
+            }
+        });
+        mToolbar.setTitle(getTitleString());
+        mToolbar.setTitleTextColor(Color.WHITE);
+    }
+
+}
+
---
+++ BarbeBLE/src/main/java/com/horanet/BarbeBLE/Constants.java
@@ -0,0 +1,36 @@
+package com.horanet.BarbeBLE;
+
+import java.util.UUID;
+
+/**
+ * Created by itanbarpeled on 28/01/2018.
+ */
+
+public class Constants {
+
+
+    public static final int SERVER_MSG_FIRST_STATE = 1;
+    public static final int SERVER_MSG_SECOND_STATE = 2;
+
+    /*
+    TODO bluetooth
+    better to use different Bluetooth Service,
+    instead of Heart Rate Service:
+    https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.service.heart_rate.xml.
+
+    maybe Object Transfer Service is more suitable:
+    https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.service.object_transfer.xml
+     */
+    public static final UUID HEART_RATE_SERVICE_UUID = UUID.fromString("0000180D-0000-1000-8000-00805f9b34fb");
+    public static final UUID BODY_SENSOR_LOCATION_CHARACTERISTIC_UUID = UUID.fromString("00002A38-0000-1000-8000-00805f9b34fb");
+
+
+
+
+    private static UUID convertFromInteger(int i) {
+        final long MSB = 0x0000000000001000L;
+        final long LSB = 0x800000805f9b34fbL;
+        long value = i & 0xFFFFFFFF;
+        return new UUID(MSB | (value << 32), LSB);
+    }
+}
